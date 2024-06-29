f2379e2aecb48b09c98be9b0fc455382f7882bed
Ayush Gakre <122914231+AyushGakre@users.noreply.github.com>
2024-06-30 02:22:36 +0530

    check

--- 2.program/WCDriver.java
+++ 2.program/WCDriver.java
@@ -12,7 +12,16 @@ public int run(String args[]) throws IOException
 if (args.length < 2) {
 System.out.println("Please give valid inputs");
 return -1; }
-JobConf conf = new JobConf(WCDriver.class); FileInputFormat.setInputPaths(conf, new Path(args[0])); FileOutputFormat.setOutputPath(conf, new Path(args[1])); conf.setMapperClass(WCMapper.class); conf.setReducerClass(WCReducer.class); conf.setMapOutputKeyClass(Text.class); conf.setMapOutputValueClass(IntWritable.class); conf.setOutputKeyClass(Text.class); conf.setOutputValueClass(IntWritable.class); JobClient.runJob(conf);
+JobConf conf = new JobConf(WCDriver.class); 
+FileInputFormat.setInputPaths(conf, new Path(args[0])); 
+FileOutputFormat.setOutputPath(conf, new Path(args[1])); 
+conf.setMapperClass(WCMapper.class); 
+conf.setReducerClass(WCReducer.class); 
+conf.setMapOutputKeyClass(Text.class); 
+conf.setMapOutputValueClass(IntWritable.class); 
+conf.setOutputKeyClass(Text.class); 
+conf.setOutputValueClass(IntWritable.class); 
+JobClient.runJob(conf);
 return 0; }
 // Main Method
 public static void main(String args[]) throws Exception {
